7110082037f76513698d2b259ebe437064f0c920
tonyt321 <gravesmarcello@gmail.com>
2023-04-04 03:15:54 +0000

    hardware RS485 remove inverse bit bang

--- src/bms_main.cpp
+++ src/bms_main.cpp
@@ -6,19 +6,12 @@
 #include "settings.h"
 #include "task_queue.h"
 
-// UART RX is connected to the *BMS* White line
-// UART TX is connected to the *MB* White line
-// TX_INPUT_PIN must be soldered to the UART TX
-#define TX_INPUT_PIN 4
-// Connected to the MB B line
-#define TX_INVERSE_OUT_PIN 5
+
+
 
 namespace {
 
-// Emulate the RS485 B line by bitbanging the inverse
-// of the TX A line.
-void IRAM_ATTR txPinRiseInterrupt() { digitalWrite(TX_INVERSE_OUT_PIN, 0); }
-void IRAM_ATTR txPinFallInterrupt() { digitalWrite(TX_INVERSE_OUT_PIN, 1); }
+
 
 HardwareSerial Serial(0);
 }  // namespace
@@ -37,16 +30,10 @@ void bms_setup() {
   Serial.begin(115200);
 
   // The B line idle is 0
-  digitalWrite(TX_INVERSE_OUT_PIN, 0);
-  pinMode(TX_INVERSE_OUT_PIN, OUTPUT);
 
-  pinMode(TX_INPUT_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(TX_INPUT_PIN), txPinRiseInterrupt,
-                  RISING);
-  attachInterrupt(digitalPinToInterrupt(TX_INPUT_PIN), txPinFallInterrupt,
-                  FALLING);
+
 
   relay->addReceivedPacketCallback([](BmsRelay *, Packet *packet) {
     static uint8_t ledState = 0;
